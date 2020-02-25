7ac5c0527c3d516c1937e5772c9bbd3d7868a5c9
Stephen Ramsay <rabrandom@gmail.com>
2020-02-25 10:47:37 +0000

    spring files generated

---
+++ src/main/java/com/employee/example/employeeLab/EmployeeLabApplication.java
@@ -0,0 +1,13 @@
+package com.employee.example.employeeLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee/example/employeeLab/EmployeeLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.example.employeeLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
